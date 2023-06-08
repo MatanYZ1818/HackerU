63879559a6dd1dac33e20ad08c613a25404b3788
MatanYZ1818 <matniredon10@Gmail.com>
2023-06-08 14:37:14 +0300

    added

---
+++ lesson24/ts/Gallery.ts
@@ -0,0 +1,8 @@
+class Gallery{
+    constructor(private elementID:string,private images:string[]){
+        const elem=document.getElementById(elementID)
+
+        const img =document.createElement("img")
+        img.src
+    }
+}
