5876b88f795df45092edfe5372c8239a6fe05bd3
MatanYZ1818 <matniredon10@Gmail.com>
2023-07-06 15:58:17 +0300

    starting react

---
+++ hello-react/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar(){
+    return(
+        <nev>
+            <ul>
+                <li><a href="/">בית</a></li>
+                <li><a href="/counter">מונה</a></li>
+                <li><a href="/gallery">גלריה</a></li>
+                <li><a href="/contact">צור קשר</a></li>
+            </ul>
+        </nev>
+    )
+}
+export default Navbar
