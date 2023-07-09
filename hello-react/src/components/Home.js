35c14e64326b0d51066178eb2223961d2f3b850c
MatanYZ1818 <matniredon10@Gmail.com>
2023-07-09 15:44:22 +0300

    added

--- hello-react/src/components/Home.js
+++ hello-react/src/components/Home.js
@@ -1,5 +1,15 @@
+import Numbers from "./Numbers"
+
 function Home() {
-    return <h2>ברוכים הבאים!</h2>;
+    return(
+        <>
+            <h2>ברוכים הבאים!</h2>;
+            <Numbers min={10} max={50} />
+            <Numbers min={30} max={75} />
+            <Numbers min={70} max={100} />
+
+        </>
+    )
 }
 
 export default Home;
--- hello-react/src/components/Navbar.js
+++ hello-react/src/components/Navbar.js
@@ -1,15 +1,17 @@
-import './Navbar.css'
+import './Navbar.css';
 
-function Navbar(){
-    return(
-        <nev>
+function Navbar() {
+    return (
+        <nav>
             <ul>
                 <li><a href="/">בית</a></li>
                 <li><a href="/counter">מונה</a></li>
                 <li><a href="/gallery">גלריה</a></li>
+                <li><a href="/settings">גלריה</a></li>
                 <li><a href="/contact">צור קשר</a></li>
             </ul>
-        </nev>
+        </nav>
     )
 }
-export default Navbar
+
+export default Navbar;
