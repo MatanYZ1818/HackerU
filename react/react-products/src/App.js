0a5220d5aad5d7abee33aafee389f47a6ea53f3d
MatanYZ1818 <matniredon10@Gmail.com>
2023-07-20 10:57:31 +0300

    added

--- react/react-products/src/App.js
+++ react/react-products/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user,setUser]=useState()
+        const [isLogged,setIsLogged]=useState()
+
+        useEffect(()=>{
+        fetch("https://api.shipap.co.il/login", {
+            credentials: 'include',
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.status == 'success') {
+                setUser(data.user);
+                setIsLogged(true)
+            } else {
+                setUser();
+                setIsLogged(false)
+            }
+        });
+    },[])
+  
+
+    return (
+        <div className="App">
+            <h1>ניהול מוצרים</h1>
+
+            <div className="frame">
+                {
+                    isLogged === undefined ? <p>טוען....</p> : isLogged ? <p>מחובר</p> : <p>להקיא</p>
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
