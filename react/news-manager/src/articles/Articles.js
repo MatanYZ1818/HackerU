b99741a02ba44c408ff60e8459cda6ab43ca5e42
MatanYZ1818 <matniredon10@Gmail.com>
2023-08-02 12:06:34 +0300

    added articles base page

--- react/news-manager/src/articles/Articles.js
+++ react/news-manager/src/articles/Articles.js
@@ -1,7 +1,35 @@
 import "./Articles.css"
+import { useState, useEffect } from 'react';
+import moment from 'moment';
 
 export default function Articles(){
+    
+    const [data, setArticles] = useState()
+
+    useEffect(() => {
+        fetch("https://api.shipap.co.il/articles", {
+            credentials: 'include',
+        })
+        .then(res => res.json())
+        .then(data => {
+            setArticles(data);
+        });
+    }, []);
+
     return(
-        <p>Articles</p>
+    <div className="cardFrame">
+        {
+            data.map(article=>{
+            return(
+                    <div className="card">
+                        <h4>{article.title}</h4>
+                        <p><b>כותב: </b>{article.author}</p>
+                        <p><b>תאריך פרסום: </b>{moment(article.birthday).format('DD/MM/yyy mm:hh')}</p>
+                        <button className="edit" onClick={() => removeUser(article.id)}>📝</button>
+                    </div>
+                )
+            })
+        }
+    </div>
     )
 }
